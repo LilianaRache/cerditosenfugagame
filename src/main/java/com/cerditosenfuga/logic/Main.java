a8da946a3e962336b8a7c085df0909b78e486c4f
Diego Basante <diegozambrano237@gmail.com>
2023-06-18 07:39:15 -0500

    agregando cambios al main

--- src/main/java/com/cerditosenfuga/logic/Main.java
+++ src/main/java/com/cerditosenfuga/logic/Main.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
 public class Main extends Application{
 
     public static void main(String[] args){
-        System.out.println("¡Cerditos En Fuga..!");
+        
         launch(args);
     }
     
